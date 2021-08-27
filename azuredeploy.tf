445086272ede06418689ec97b984d59fc5b8568f
NisargShah1410 <62133719+NisargShah1410@users.noreply.github.com>
2021-08-27 09:57:22 +0530

    Update azuredeploy.tf

--- azuredeploy.tf
+++ azuredeploy.tf
@@ -1,9 +1,17 @@
-#backend
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rgname"
-    storage_account_name = "storageaccoutnisarg"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
+resource "azurerm_resource_group" "example" {
+  name     = "rgname"
+  location = "westeurope"
+}
+
+module "web_app" {
+  source = "innovationnorway/web-app/azurerm"
+
+  name = "example"
+
+  resource_group_name = azurerm_resource_group.example.name
+
+  runtime = {
+    name    = "dotnetcore"
+    version = "2.2"
   }
 }
