25ebc439067ed43e2b4eb4b9c98f4603a42ee3a8
NisargShah1410 <62133719+NisargShah1410@users.noreply.github.com>
2021-08-27 10:06:43 +0530

    Update azuredeploy.tf

--- azuredeploy.tf
+++ azuredeploy.tf
@@ -23,7 +23,7 @@ resource "azurerm_resource_group" "example" {
 module "web_app" {
   source = "innovationnorway/web-app/azurerm"
 
-  name = "example"
+  name = "exampleforTF"
 
   resource_group_name = azurerm_resource_group.example.name
 
