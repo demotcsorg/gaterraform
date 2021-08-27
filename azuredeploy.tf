1cc7f972e98be644a35cf47bee41ef5ef7072405
NisargShah1410 <62133719+NisargShah1410@users.noreply.github.com>
2021-08-27 10:12:44 +0530

    Update azuredeploy.tf

--- azuredeploy.tf
+++ azuredeploy.tf
@@ -6,7 +6,7 @@ provider "azurerm" {
 #backend
 terraform {
   backend "azurerm" {
-    resource_group_name  = "example-resources"
+    resource_group_name  = "rgname"
     storage_account_name = "storageaccoutnisarg"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
