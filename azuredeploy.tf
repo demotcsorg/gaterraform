8acfc414c1b964c37e549d7f66930fa9e7ea5067
NisargShah1410 <62133719+NisargShah1410@users.noreply.github.com>
2021-08-27 11:13:23 +0530

    Update azuredeploy.tf

--- azuredeploy.tf
+++ azuredeploy.tf
@@ -16,8 +16,8 @@ terraform {
 
 
 resource "azurerm_resource_group" "example" {
-  name     = "rgnamedemo"
-  location = "uksouth"
+  name     = "rgname"
+  location = "westeurope"
 }
 
 module "web_app" {
